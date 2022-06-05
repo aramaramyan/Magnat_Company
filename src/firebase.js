dbf4a8e453b71271a06d37bfdf5b1894b5867a63
aramaramyan <aramaramyan1997@mail.ru>
2022-06-05 18:51:28 +0400

    feat(firebase): add firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import {initializeApp} from "firebase/app";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyBPu9obBuVk441Krt55XjKgOsyik23leJo",
+  authDomain: "magnat-company.firebaseapp.com",
+  projectId: "magnat-company",
+  storageBucket: "magnat-company.appspot.com",
+  messagingSenderId: "925275191716",
+  appId: "1:925275191716:web:9539476ab28492e3cda931"
+});
+
+export default firebaseApp;
+
+
+
